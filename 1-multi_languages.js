1ed17e1301d9314310002ca9cb654d1607044c60
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2025-11-10 04:41:18 +0000

    update language

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,8 +1,8 @@
 // languages 
-const line1 = "C ic fun"
-const line2 = "python is cool"
-const line3 = "Javascript is amazing"
+const line1 = "C is fun";
+const line2 = "python is cool";
+const line3 = "Javascript is amazing";
 
-cosole.log(line1)
-console.log(line2)
-console.log(line3)
+console.log(line1);
+console.log(line2);
+console.log(line3);
