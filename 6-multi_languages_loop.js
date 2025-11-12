f344fba810d53bcd35b0b3d669492b90ba771708
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2025-11-12 10:48:39 +0000

    6

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+// Array of strings
+const languages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+// Loop through array and print each line
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
